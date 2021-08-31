ce5c39061c435bafffd1863c0614aabec88bc6fc
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 02:33:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NestedIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=5, b=10, c=4;
+            if (a>b)
+            {
+                if (a>c)
+                {
+                    Console.WriteLine("{0} is The largest",a);
+                }
+                else{
+                    Console.WriteLine("{0} is The largest", c);
+                }
+            }
+            else
+            {
+                if(b>c){
+                    Console.WriteLine("{0} is The largest", b);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is The largest", c);
+                }
+            }
+        }
+    }
+}
